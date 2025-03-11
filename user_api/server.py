1a109df357d2f2880303b6db37246f78646bbee5
Nir Fakiro <fakironir@gmail.com>
2025-03-11 22:26:16 +0200

    Add automatic termination

--- user_api/server.py
+++ user_api/server.py
@@ -1,9 +1,18 @@
 from flask import Flask
 from flask_cors import CORS
 from user_routers import user_router
+import os
+import signal
+
+
 
 app = Flask(__name__)
 CORS(app)
+
+@app.route('/stop_server')
+def stop_server():
+ os.kill(os.getpid(), signal.CTRL_C_EVENT)
+ return 'Server stopped'
 
 app.register_blueprint(user_router, url_prefix='/api')
 
